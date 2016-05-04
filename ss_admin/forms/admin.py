770357f266b3b4e97c4a9ae0d3562e0254dcb9f4
arrti <imxmwd@gmail.com>
2016-05-04 16:51:11 +0800

    add validate verification code func

--- ss_admin/forms/admin.py
+++ ss_admin/forms/admin.py
@@ -8,12 +8,16 @@ from flask.ext.wtf import Form
 
 class login(Form):
     email = StringField('email', validators=[
-        validators.DataRequired(),
-        validators.Length(min=4),
-        validators.Email()
+        validators.DataRequired(message='email is required'),
+        validators.Length(min=4, message='email is too short'),
+        validators.Email(message='email is invalid')
     ])
 
     password = StringField('password', validators=[
-        validators.DataRequired(),
-        validators.Length(min=6, max=32)
+        validators.DataRequired(message='password is required'),
+        validators.Length(min=6, max=32, message='password is required 6~32 characters')
+    ])
+
+    vcode = StringField('vcode', validators=[
+        validators.DataRequired(message='verification code is required')
     ])
