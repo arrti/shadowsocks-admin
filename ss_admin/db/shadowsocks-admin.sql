b90f6932c4d4914213e136dd4c8dfb852d589b80
arrti <imxmwd@gmail.com>
2016-05-16 21:49:43 +0800

    add procedure and event to reset transfer on the 1st of each month at 01:00 AM

--- ss_admin/db/shadowsocks-admin.sql
+++ ss_admin/db/shadowsocks-admin.sql
@@ -36,3 +36,30 @@ ALTER TABLE `admin` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- INSERT INTO `admin` (`id`,`email`,`password`) VALUES
 -- ('','','123456');
+
+-- ---
+-- PROCEDURE reset transfer
+-- ---
+
+DELIMITER $$
+
+DROP PROCEDURE IF EXISTS `shadowsocks`.`reset_transfer`$$
+
+CREATE PROCEDURE `shadowsocks`.`reset_transfer`()
+
+    BEGIN
+    UPDATE shadowsocks.user SET d=0, u=0;
+    END$$
+
+DELIMITER ;
+
+-- ---
+-- EVENT reset transfer on the 1st of each month at 01:00 AM
+-- ---
+
+DROP EVENT IF EXISTS e_sa_reset;
+
+CREATE EVENT e_sa_reset
+ON SCHEDULE EVERY 1 MONTH  STARTS
+DATE_ADD(DATE_ADD(DATE_SUB(CURDATE(),INTERVAL DAY(CURDATE())-1 DAY), INTERVAL 1 MONTH),INTERVAL 1 HOUR)
+DO call shadowsocks.reset_transfer();
